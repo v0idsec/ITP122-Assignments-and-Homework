6e0d4f090d74a08e05e68800d35fbe6f0b336dcd
v0idsec <131359172+v0idsec@users.noreply.github.com>
2023-04-27 09:56:31 +0930

    Create ReverseYourName.py

---
+++ ReverseYourName.py
@@ -0,0 +1,10 @@
+# Create variables that accept user input to be saved
+
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+
+# printing in reverse order
+# We will make the print command pring LastName fist, then FirstName
+# We can also include a space with " "
+
+print(LastName + " " + FirstName)
